41cab077a8b7156906c2ba78240fca7d11c25509
Nathalia <nathaliacristina20@hotmail.com>
2021-01-21 20:58:36 -0300

    animate button component

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,15 +1,34 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  template: `<button [@stretch]="buttonState" (click)="onClick()">Click</button>`,
+  styleUrls: ['./button.component.css'],
+  animations: [
+    trigger('stretch', [
+      state('normal', style({ width: '60px' })),
+      state('stretched', style({ width: '120px' })),
+      transition('normal => stretched', animate('300ms')),
+      transition('stretched => normal', animate('500ms')),
+    ])
+  ]
 })
 export class ButtonComponent implements OnInit {
 
   constructor() { }
 
+  buttonState = 'normal';
+
   ngOnInit(): void {
+  }
+
+  onClick(){
+    if (this.buttonState === "normal"){
+      this.buttonState = 'stretched';
+    } else {
+      this.buttonState = 'normal';
+    }
   }
 
 }
